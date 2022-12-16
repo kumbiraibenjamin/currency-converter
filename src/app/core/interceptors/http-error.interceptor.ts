359d5364e606ae8f233567dd5c56099f1259960b
kumbiraibenjamin <32431147+kumbiraibenjamin@users.noreply.github.com>
2022-12-16 12:16:23 +0400

    feat: updated error interceptor

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -7,8 +7,16 @@ import {
   HttpHeaders,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { catchError, Observable, retry, throwError } from 'rxjs';
-import { ToastService } from 'src/app/shared/toast/toast.service';
+import {
+  catchError,
+  map,
+  Observable,
+  of,
+  retry,
+  switchMap,
+  throwError,
+} from 'rxjs';
+import { ToastService } from 'src/app/shared/components/toast/toast.service';
 import { Router } from '@angular/router';
 
 @Injectable()
@@ -22,112 +30,17 @@ export class ErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<T>> {
     return next.handle(req).pipe(
       retry(1),
-      catchError((error: HttpErrorResponse) => {
-        let errorMessage = '';
-
-        if (error.error instanceof ErrorEvent) {
-          // client-side error
-          errorMessage = `Error: ${error.error.message}`;
-        } else {
-          // server-side error
-
-          switch (error.status) {
-            case 404:
-              this.toastService.showDanger(
-                'The requested resource does not exist.'
-              );
-              break;
-
-            case 101:
-              this.toastService.showDanger(
-                'No API Key was specified or an invalid API Key was specified.'
-              );
-              break;
-
-            case 103:
-              this.toastService.showDanger(
-                'The requested API endpoint does not exist.'
-              );
-              break;
-            case 104:
-              this.toastService.showDanger(
-                'The maximum allowed API amount of monthly API requests has been reached.'
-              );
-              break;
-            case 105:
-              this.toastService.showDanger(
-                'The current subscription plan does not support this API endpoint.'
-              );
-              break;
-            case 106:
-              this.toastService.showDanger(
-                'The current request did not return any results.'
-              );
-              break;
-            case 102:
-              this.toastService.showDanger(
-                'The account this API request is coming from is inactive.'
-              );
-              break;
-            case 201:
-              this.toastService.showDanger(
-                'An invalid base currency has been entered.'
-              );
-              break;
-            case 202:
-              this.toastService.showDanger(
-                'One or more invalid symbols have been specified.'
-              );
-              break;
-            case 301:
-              this.toastService.showDanger(
-                'No date has been specified. [historical]'
-              );
-              break;
-            case 302:
-              this.toastService.showDanger(
-                'An invalid date has been specified. [historical, convert]'
-              );
-              break;
-            case 403:
-              this.toastService.showDanger(
-                'No or an invalid amount has been specified. [convert]'
-              );
-              break;
-            case 501:
-              this.toastService.showDanger(
-                'No or an invalid timeframe has been specified. [timeseries]'
-              );
-              break;
-            case 502:
-              this.toastService.showDanger(
-                'No or an invalid "start_date" has been specified. [timeseries, fluctuation]'
-              );
-              break;
-            case 503:
-              this.toastService.showDanger(
-                'No or an invalid "end_date" has been specified. [timeseries, fluctuation]'
-              );
-              break;
-            case 504:
-              this.toastService.showDanger(
-                'An invalid timeframe has been specified. [timeseries, fluctuation]'
-              );
-              break;
-            case 505:
-              this.toastService.showDanger(
-                'The specified timeframe is too long, exceeding 365 days. [timeseries, fluctuation]'
-              );
-              break;
-
-            default:
-              this.toastService.showDanger(
-                'Unknown error, please check connectivity and restart the application'
-              );
-              break;
+      switchMap((res: any) => {
+        if (res?.body?.success === false) {
+          if (res?.body?.error?.info) {
+            return throwError(() => new Error(res?.body?.error?.info));
           }
         }
-        return throwError(() => new Error(errorMessage));
+        return of(res);
+      }),
+      catchError((error: HttpErrorResponse) => {
+        this.toastService.showDanger(error.error.message);
+        return throwError(() => error);
       })
     );
   }
